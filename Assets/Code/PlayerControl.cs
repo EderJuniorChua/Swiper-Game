e7582606f92a59540ffe36c0c6e17e0bde1b050e
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-08-03 20:15:54 +0800

    tested swipe Character Prototype and working, needs a lot of refactoring though

--- Assets/Code/PlayerControl.cs
+++ Assets/Code/PlayerControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    //comment again
+    //this is a new branch
     // this is to make player move
     private float dir = 1;
     public float speed;
